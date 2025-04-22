f249d20a66e7102002dfd96bd98a9d1466de601a
yzemlyak <yzemlyak@gmail.com>
2025-04-22 02:01:53 +0000

    midway

--- app.rb
+++ app.rb
@@ -1,11 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
 require "http"
+require "dotenv/load"
+
+api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
 
 get("/") do
-  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=6069b11e44c9f01bfbd63f1f29366f15")
-
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
   @string_response = @raw_response.to_s
   @parsed_response = JSON.parse(@string_response)
+  @currencies = @parsed_response.fetch("currencies")
   erb(:homepage)
 end
+
+get("/:first_symbol") do
+  @the_symbol = params.fetch("first_symbol")
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+  @string_response = @raw_response.to_s
+  @parsed_response = JSON.parse(@string_response)
+  @currencies = @parsed_response.fetch("currencies")
+  erb(:step_one)
+end
+
--- env_test.rb
+++ env_test.rb
@@ -1,3 +1,3 @@
 require "dotenv/load"
 
-pp ENV.fetch("API_KEY")
+pp ENV.fetch("EXCHANGE_RATE_KEY")

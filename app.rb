0b3d15eff66230febff253112e38478cd4b2e6d9
yzemlyak <yzemlyak@gmail.com>
2025-04-22 01:15:47 +0000

    Early hash

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=6069b11e44c9f01bfbd63f1f29366f15")
+
+  @string_response = @raw_response.to_s
+  @parsed_response = JSON.parse(@string_response)
+  erb(:homepage)
 end
